422413e73134839d5f19216db2930354b3096a82
Shubham <ss080994@gmail.com>
2018-07-05 00:50:20 +0530

    Service changes

--- src/main/java/com/ssp/storage/controller/UserController.java
+++ src/main/java/com/ssp/storage/controller/UserController.java
@@ -1,27 +1,29 @@
 package com.ssp.storage.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.ssp.storage.domain.User;
 import com.ssp.storage.error.AcgError;
 import com.ssp.storage.exception.UserException;
 import com.ssp.storage.service.IUserService;
+import com.ssp.storage.vo.Signup;
 import com.ssp.storage.web.ResponseEntity;
 
-
-@RestController
-@RequestMapping("/api/user")
+@Controller
+@RequestMapping("/")
 public class UserController {
 
 	Logger logger = LoggerFactory.getLogger(UserController.class);
@@ -32,7 +34,7 @@ public class UserController {
 	@Autowired
 	IUserService userService;
 
-	@GetMapping("/")
+	@GetMapping("/user")
 	public ResponseEntity<?> authenticateUser(@RequestHeader(name = "username") String username,
 			@RequestHeader(name = "password") String password) {
 		try {
@@ -47,9 +49,10 @@ public class UserController {
 		}
 	}
 
-	@PostMapping(value = "/addUser", produces = MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> addCustomer( @RequestBody User user) {
-		try {
+	@PostMapping(value = "/signup" /*, consumes= {"application/x-www-form-urlencoded;charset=UTF-8"}*/)
+	public void addCustomer(@ModelAttribute Signup signup/*@RequestBody User user*/) {
+		System.out.println(signup);
+		/*try {
 			return new ResponseEntity<>(userService.addUser(user), HttpStatus.OK);
 		} catch (UserException e) {
 			logger.error(e.getMessage(), e);
@@ -58,6 +61,16 @@ public class UserController {
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		}*/
+	}
+
+	@GetMapping
+	public String e(Model model) {
+		List<String> questions = new ArrayList<>();
+		questions.add("What time of the day were you born?");
+		questions.add("What is the middle name of your youngest cousin?");
+		questions.add("What was the last name of your first grade teacher");
+		model.addAttribute("securityQuestions", questions);
+		return "signup";
 	}
 }
